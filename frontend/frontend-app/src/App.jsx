536c0c7a6e1c36b2f29a51e8417b36b5701f56e0
SebaSzym777 <s.szymanski2045@gmail.com>
2025-07-02 00:23:40 +0200

    test webbook 3

--- frontend/frontend-app/src/App.jsx
+++ frontend/frontend-app/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h2>Panel logowaniaaaaaaaaaaaaa test</h2>
+      <h2>Panel logowaniaaaaaaaaaaaaa test 2</h2>
 
       <input
         type="text"
